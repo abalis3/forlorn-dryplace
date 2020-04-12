a25f5030f32a2e18ed36d1aa2b521a77587860ce
Abalis3 <novados24@gmail.com>
2020-04-12 13:34:34 -0700

    try forcing Color as cast

--- include/raylib/BoundingBox.hpp
+++ include/raylib/BoundingBox.hpp
@@ -42,7 +42,7 @@ namespace raylib {
             return *this;
         }
 
-		inline BoundingBox& Draw(::Color color = WHITE) {
+		inline BoundingBox& Draw(::Color color = (Color){ 255, 255, 255, 255 }) {
 			DrawBoundingBox(*this, color);
 			return *this;
 		}
