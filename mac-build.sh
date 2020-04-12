b78e15968f0d68a6c2f4e3582d4d53bd229e5d47
Abalis3 <novados24@gmail.com>
2020-04-12 15:02:14 -0700

    make mac build c++11 compliant

--- mac-build.sh
+++ mac-build.sh
@@ -1,4 +1,4 @@
 if [ -d bin ]; then rm -rf bin; fi
 mkdir bin
 
-g++ -DRAYLIB_CPP_NO_MATH=1 -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL -I raylib/include -I include raylib/lib/libraylib.a src/*.cpp -o bin/forlorn-dryplace
+g++ -std=c++11 -DRAYLIB_CPP_NO_MATH=1 -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL -I raylib/include -I include raylib/lib/libraylib.a src/*.cpp -o bin/forlorn-dryplace
