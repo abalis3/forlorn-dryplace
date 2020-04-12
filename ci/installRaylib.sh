c060f25c6e20f04d51cb1f1c892ef74c88a6fbe7
Abalis3 <novados24@gmail.com>
2020-04-11 21:54:17 -0700

    hopefully fixed gl issue

--- ci/installRaylib.sh
+++ ci/installRaylib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev #libgl1-mesa-dev libglu1-mesa-dev
+sudo apt-get install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev
+sudo ln -s /usr/lib/x86_64-linux-gnu/libGL.so.1 /usr/lib/libGL.so
 
 RAYLIB_URL=$(curl -s https://api.github.com/repos/raysan5/raylib/releases/tags/3.0.0 \
     | grep browser_download_url \
