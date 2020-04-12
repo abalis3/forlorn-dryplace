e697c40a5d826a313c88c6a9842fc26794e88f28
Abalis3 <novados24@gmail.com>
2020-04-12 15:13:03 -0700

    add mac executable artifact upload

--- ci/installLinuxDependencies.sh
+++ ci/installLinuxDependencies.sh
@@ -2,13 +2,7 @@
 
 sudo apt-get install mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev #libasound2-dev
 
-RAYLIB_URL=$(curl -s https://api.github.com/repos/raysan5/raylib/releases/tags/3.0.0 \
-    | grep browser_download_url \
-    | grep Linux \
-    | grep 64 \
-    | cut -d '"' -f 4)
-
-wget $RAYLIB_URL
+wget https://github.com/raysan5/raylib/releases/download/3.0.0/raylib-3.0.0-Linux-amd64.tar.gz
 
 mkdir -p raylib
 tar xvzf raylib-3.0.0-Linux-amd64.tar.gz --strip 1 --directory raylib
--- ci/installMacDependencies.sh
+++ ci/installMacDependencies.sh
@@ -5,6 +5,3 @@ wget https://github.com/raysan5/raylib/releases/download/3.0.0/raylib-3.0.0-macO
 mkdir -p raylib
 tar xvzf raylib-3.0.0-macOS.tar.gz --strip 1 --directory raylib
 rm raylib-3.0.0-macOS.tar.gz
-
-# Special tweak to raylib to work with mac compile (override turning off compound literal for cpp)
-#sed -i '' 's/CLITERAL(Color)/(Color)/g' raylib/include/raylib.h
