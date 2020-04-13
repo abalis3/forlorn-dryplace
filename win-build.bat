ff76ae9c02db0fa179121495091ee02cd9c05b9d
Abalis3 <novados24@gmail.com>
2020-04-12 17:59:24 -0700

    try another method

--- win-build.bat
+++ win-build.bat
@@ -4,7 +4,7 @@ mkdir bin
 
 if "%*"=="CI" (
     g++ -m32 -DRAYLIB_CPP_NO_MATH=1 -I raylib/include/ -I include/ -L raylib/lib src/*.cpp -lopengl32 -static -lraylib_static -lpthread -lwinmm -lgdi32 -o bin/forlorn-dryplace.exe
-    echo %ERRORLEVEL%
+    echo ERRORLEVEL
     echo.
     echo Built executable for CI
 ) else (
