bd562a37c903ed43b3b95d16d2ceca01c6241491
Abalis3 <novados24@gmail.com>
2020-04-12 17:47:52 -0700

    make CI job fail if win compile fails

--- win-build.bat
+++ win-build.bat
@@ -3,7 +3,8 @@ rmdir /s /q bin
 mkdir bin
 
 if "%*"=="CI" (
-    g++ -m64 -DRAYLIB_CPP_NO_MATH=1 -I raylib/include/ -I include/ -L raylib/lib src/*.cpp -lopengl32 -static -lraylib_static -lpthread -lwinmm -lgdi32 -o bin/forlorn-dryplace.exe    
+    g++ -DRAYLIB_CPP_NO_MATH=1 -I raylib/include/ -I include/ -L raylib/lib src/*.cpp -lopengl32 -static -lraylib_static -lpthread -lwinmm -lgdi32 -o bin/forlorn-dryplace.exe    
+    if %errorlevel% neq 0 exit /b %errorlevel%
     echo.
     echo Built executable for CI
 ) else (
