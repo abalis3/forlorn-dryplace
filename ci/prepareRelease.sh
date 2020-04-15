2019143f936995a51038ebafa23373b4ad3de9e9
Abalis3 <novados24@gmail.com>
2020-04-14 23:26:59 -0700

    fix: Force executable file type before archiving for release

--- ci/prepareRelease.sh
+++ ci/prepareRelease.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 cd linux-executable
+chmod +x forbidden-desert
 ls -la
 tar -czpvf forbidden-desert-ubuntu-v$1.tar.gz *
 cd ..
 
 cd macOS-executable
+chmod +x forbidden-desert
 ls -la
 tar -czpvf forbidden-desert-macOS-v$1.tar.gz *
 cd ..
