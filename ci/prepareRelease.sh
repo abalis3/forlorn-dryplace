91aa46348d6fd6c29b58f02a342c3825f7707702
Abalis3 <novados24@gmail.com>
2020-04-14 23:44:23 -0700

    Remove extra log from prepareRelease script

--- ci/prepareRelease.sh
+++ ci/prepareRelease.sh
@@ -2,13 +2,11 @@
 
 cd linux-executable
 chmod +x forbidden-desert
-ls -la
 tar -czpvf forbidden-desert-ubuntu-v$1.tar.gz *
 cd ..
 
 cd macOS-executable
 chmod +x forbidden-desert
-ls -la
 tar -czpvf forbidden-desert-macOS-v$1.tar.gz *
 cd ..
 
