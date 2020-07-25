06c0574ea8f3ace44b0ace3454d44482f1f3678d
Robert Brajcich <robertbrajcich@gmail.com>
2020-07-24 19:41:10 -0700

    refactor: Move game client into subdir to prepare for server

--- client/win-build.bat
+++ client/win-build.bat
@@ -5,9 +5,9 @@ mkdir bin
 mkdir "src/pbuf/generated"
 
 
-start /B /WAIT ./vcpkg/installed/x86-windows/tools/protobuf/protoc -I ./src/pbuf --cpp_out=./src/pbuf/generated ./src/pbuf/*.proto
+start /B /WAIT ../vcpkg/installed/x86-windows/tools/protobuf/protoc -I ./src/pbuf --cpp_out=./src/pbuf/generated ./src/pbuf/*.proto
 msbuild /verbosity:quiet /p:OutDir=bin/ /p:configuration=release forbidden-desert.vcxproj
 rmdir /s /q release
-copy .\vcpkg\installed\x86-windows\bin\raylib.dll .\bin > NUL
-copy .\vcpkg\installed\x86-windows\bin\libprotobuf.dll .\bin > NUL
+copy ..\vcpkg\installed\x86-windows\bin\raylib.dll .\bin > NUL
+copy ..\vcpkg\installed\x86-windows\bin\libprotobuf.dll .\bin > NUL
 echo Built executable in 'bin/forbidden-desert.exe'
--- linux-build.sh
+++
@@ -1,8 +0,0 @@
-if [ -d bin ]; then rm -rf bin; fi
-if [ -d src/pbuf/generated ]; then rm -rf src/pbuf/generated; fi
-mkdir bin
-mkdir src/pbuf/generated
-
-./vcpkg/installed/x64-linux/tools/protobuf/protoc -I ./src/pbuf --cpp_out=./src/pbuf/generated ./src/pbuf/*.proto
-g++ -DRAYLIB_CPP_NO_MATH=1 -I vcpkg/installed/x64-linux/include/ -I include/ -L vcpkg/installed/x64-linux/lib/ src/*.cpp ./src/pbuf/generated/*.cc -no-pie -Wl,-Bstatic -lraylib -lprotobuf -Wl,-Bdynamic -lGL -lm -lpthread -ldl -lX11 -lXrandr -lXinerama -lXi -lXxf86vm -lXcursor -o bin/forbidden-desert
-
