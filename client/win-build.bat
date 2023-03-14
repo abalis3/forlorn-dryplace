82793a832eeaa1454f834fc39408e6d2b7aedc94
Abalis3 <novados24@gmail.com>
2023-03-13 23:33:44 -0600

    ci: Stripping down win-build.bat further to find hang

--- client/win-build.bat
+++ client/win-build.bat
@@ -1,4 +1,3 @@
-@ECHO OFF
 rmdir /s /q bin > NUL 2>&1
 rmdir /s /q "src/pbuf/generated" > NUL 2>&1
 rmdir /s /q "../shared-src/pbuf/generated" > NUL 2>&1
@@ -6,10 +5,3 @@ mkdir bin
 mkdir "src/pbuf/generated"
 mkdir "../shared-src/pbuf/generated"
 
-
-start /B /WAIT ../vcpkg_installed/x86-windows/tools/protobuf/protoc -I ./src/pbuf --cpp_out=./src/pbuf/generated ./src/pbuf/*.proto
-start /B /WAIT ../vcpkg_installed/x86-windows/tools/protobuf/protoc -I ../shared-src/pbuf --cpp_out=../shared-src/pbuf/generated ../shared-src/pbuf/*.proto
-rmdir /s /q release
-copy ..\vcpkg\installed\x86-windows\bin\raylib.dll .\bin > NUL
-copy ..\vcpkg\installed\x86-windows\bin\libprotobuf.dll .\bin > NUL
-echo Built executable in 'bin/forbidden-desert.exe'
