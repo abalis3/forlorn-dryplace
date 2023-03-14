2e8b83a329912d53b7d47148d14dcd6d1b185857
Abalis3 <novados24@gmail.com>
2023-03-13 23:53:59 -0600

    ci: Try targeting x86 windows explicitly

--- client/win-build.bat
+++ client/win-build.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 rmdir /s /q bin > NUL 2>&1
 rmdir /s /q "src/pbuf/generated" > NUL 2>&1
 rmdir /s /q "../shared-src/pbuf/generated" > NUL 2>&1
@@ -5,3 +6,11 @@ mkdir bin
 mkdir "src/pbuf/generated"
 mkdir "../shared-src/pbuf/generated"
 
+
+start /B /WAIT ../vcpkg_installed/x86-windows/tools/protobuf/protoc -I ./src/pbuf --cpp_out=./src/pbuf/generated ./src/pbuf/*.proto
+start /B /WAIT ../vcpkg_installed/x86-windows/tools/protobuf/protoc -I ../shared-src/pbuf --cpp_out=../shared-src/pbuf/generated ../shared-src/pbuf/*.proto
+msbuild /verbosity:quiet /p:OutDir=bin/ /p:configuration=release forbidden-desert.vcxproj
+rmdir /s /q release
+copy ..\vcpkg\installed\x86-windows\bin\raylib.dll .\bin > NUL
+copy ..\vcpkg\installed\x86-windows\bin\libprotobuf.dll .\bin > NUL
+echo Built executable in 'bin/forbidden-desert.exe'
