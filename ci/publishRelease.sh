6946d5fb658536eb27bec5f662cc818284ba54ff
Abalis3 <novados24@gmail.com>
2023-09-15 21:19:39 -0600

    fix: Explicitly push all tags to dockerhub now

--- ci/publishRelease.sh
+++ ci/publishRelease.sh
@@ -2,4 +2,4 @@
 
 # Push built server container tags to docker hub
 echo $DOCKERHUB_TOKEN | docker login -u abalis3 --password-stdin
-docker push abalis3/forbidden-desert-server
+docker push --all-tags abalis3/forbidden-desert-server
