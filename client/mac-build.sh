91e9391c361b2ff4f23bd7767471e5ac075f5b2f
Abalis3 <novados24@gmail.com>
2024-02-12 22:29:37 -0700

    fix: Force mac to build for version it's running on

--- client/mac-build.sh
+++ client/mac-build.sh
@@ -1,4 +1,5 @@
 ARCH=$(uname -m)
+OSX_VERSION=$(sw_vers -productVersion)
 
 if [ "$ARCH" == "arm64" ]; then
     VC_TUPLE=arm64-osx
@@ -15,5 +16,5 @@ mkdir ../shared-src/pbuf/generated
 
 ../vcpkg_installed/${VC_TUPLE}/tools/protobuf/protoc -I ./src/pbuf --cpp_out=./src/pbuf/generated ./src/pbuf/*.proto
 ../vcpkg_installed/${VC_TUPLE}/tools/protobuf/protoc -I ../shared-src/pbuf --cpp_out=../shared-src/pbuf/generated ../shared-src/pbuf/*.proto
-g++ -std=c++17 -DOSX_RELEASE_BUILD -DRAYLIB_CPP_NO_MATH=1 -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL -I ../vcpkg_installed/${VC_TUPLE}/include/ -I include ../vcpkg_installed/${VC_TUPLE}/lib/*.a -I ../shared-src/ src/*.cpp ../shared-src/*.cpp src/pbuf/generated/*.cc ../shared-src/pbuf/generated/*.cc -o bin/forbidden-desert
+g++ -std=c++17 -mmacosx-version-min=${OSX_VERSION} -DOSX_RELEASE_BUILD -DRAYLIB_CPP_NO_MATH=1 -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL -I ../vcpkg_installed/${VC_TUPLE}/include/ -I include ../vcpkg_installed/${VC_TUPLE}/lib/*.a -I ../shared-src/ src/*.cpp ../shared-src/*.cpp src/pbuf/generated/*.cc ../shared-src/pbuf/generated/*.cc -o bin/forbidden-desert
 
